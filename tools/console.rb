9f0382ed8360fff90cc44bcc8675106ed9fc7d2b
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-06-11 13:44:13 -0500

    updated solution to art gallery pair programming - Eten and Dani

--- tools/console.rb
+++ tools/console.rb
@@ -13,10 +13,15 @@ gallery2 = Gallery.new("Gallery2", "Addis Ababa")
 gallery3 = Gallery.new("Gallery3", "Chicago")
 gallery4 = Gallery.new("Gallery4", "New Jersey")
 
-painting1 = Painting.new("Mona Lisa", 99999, artist1, gallery1) 
-painting2 = Painting.new("El Salvador", 2, artist2, gallery1)
-painting3 = Painting.new("Water Serpent", 50000, artist3, gallery1)
-painting4 = Painting.new("Starry Night", 5, artist4, gallery1)
+painting1 = Painting.new("Mona Lisa", 99999, artist4, gallery1) 
+painting2 = Painting.new("El Salvador", 2, artist3, gallery3)
+painting3 = Painting.new("Water Serpent", 50000, artist2, gallery2)
+painting4 = Painting.new("Starry Night", 5, artist1, gallery1)
+
+painting5 = Painting.new("Mona Lisa", 122, artist1, gallery4) 
+painting6 = Painting.new("El Salvador", 1, artist2, gallery2)
+painting7 = Painting.new("Water Serpent", 12, artist3, gallery1)
+painting8 = Painting.new("Starry Night", 3435, artist4, gallery3)
 
 
 
